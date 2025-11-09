e5b82464db9bc4aeb16b365d079667010600df55
Jacopo Donati <33698919+cantaprete@users.noreply.github.com>
2025-11-09 16:43:31 +0100

    Migliorato il fracking translator

---
+++ js/mescola_ragazzo.js
@@ -0,0 +1,21 @@
+const delay = ms => new Promise(res => setTimeout(res, ms))
+
+function mescola() {
+    let parole = document.querySelectorAll('span')
+    let parola = parole[Math.floor(Math.random() * parole.length)]
+    if (parola.dataset.currLang === 'en') {
+        parola.textContent = parola.dataset.langIt
+        parola.dataset.currLang = 'it'
+    } else {
+        parola.textContent = parola.dataset.langEn
+        parola.dataset.currLang = 'en'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let parole = document.querySelectorAll('span')
+    for (let i = 0; i < (parole.length / 2); i++) {
+        mescola()
+    }
+    window.setInterval(mescola, 1000)
+})
