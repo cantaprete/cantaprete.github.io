d454c2d5b1b4779a06561027d68f5ccb6ff5640b
Jacopo Donati <33698919+cantaprete@users.noreply.github.com>
2025-11-09 12:20:46 +0100

    Aggiunta Past Glories

---
+++ js/rummage_through_the_stock_boy.js
@@ -0,0 +1,62 @@
+function sameCalendar(year, targetYear) {
+    if (isLeapYear(year) !== isLeapYear(targetYear)) {
+        return false
+    }
+
+    if (getDayOfWeek(year, 1, 1) !== getDayOfWeek(targetYear, 1, 1)) {
+        return false
+    }
+
+    return true
+}
+
+function getDayOfWeek(year, month, day) {
+    if (month < 3) {
+        month += 12
+        year -= 1
+    }
+
+    let dayOfWeek = day
+    dayOfWeek += Math.floor(13 * (month + 1) / 5)
+    dayOfWeek += year
+    dayOfWeek += Math.floor(year / 4)
+    dayOfWeek -= Math.floor(year / 100)
+    dayOfWeek += Math.floor(year / 400)
+    dayOfWeek = dayOfWeek % 7
+
+
+    return dayOfWeek
+}
+
+function isLeapYear(year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fixTextarea();
+    let nextYear = new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+    let chosenYear = document.querySelector('#anno')
+    chosenYear.value = nextYear.getFullYear()
+    
+    let submit = document.querySelector('#trova')
+    submit.addEventListener('click', () => {
+        let outputArea = document.querySelector('#lista')
+        outputArea.value = ''
+        const cutYear = document.querySelector('#soglia').value
+        const targetYear = chosenYear.value
+        
+        for (let year = cutYear; year < targetYear; year++) {
+            if (sameCalendar(year, targetYear)) {
+                outputArea.value += year + '\r\n'
+            }
+        }
+    })
+})
+
+function fixTextarea() {
+    let textarea = document.querySelector('#lista')
+    let height = window.innerHeight - textarea.getBoundingClientRect().top
+    textarea.style.height = (height - 20) + 'px'
+}
+
+window.addEventListener('resize', fixTextarea)
