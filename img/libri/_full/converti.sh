c74d9cdd741793ffdcea341fe86c7227e1ea3cb8
Jacopo Donati <33698919+cantaprete@users.noreply.github.com>
2026-03-30 16:26:59 +0200

    Fix imagemagick

--- img/libri/_full/converti.sh
+++ img/libri/_full/converti.sh
@@ -13,19 +13,19 @@ cartella="${file%.*}"
 mkdir -p "$cartella"
 
 # PNG
-magick "$file" -filter Lanczos -resize "${larghezza_px}x" -quality 90 "$cartella/1x.png"
-magick "$file" -filter Lanczos -resize "$((larghezza_px * 2))x" -quality 90 "$cartella/2x.png"
-magick "$file" -filter Lanczos -resize "$((larghezza_px * 3))x" -quality 90 "$cartella/3x.png"
-magick "$file" -quality 90 "$cartella/full.png"
+convert "$file" -filter Lanczos -resize "${larghezza_px}x" -quality 90 "$cartella/1x.png"
+convert "$file" -filter Lanczos -resize "$((larghezza_px * 2))x" -quality 90 "$cartella/2x.png"
+convert "$file" -filter Lanczos -resize "$((larghezza_px * 3))x" -quality 90 "$cartella/3x.png"
+convert "$file" -quality 90 "$cartella/full.png"
 
 # WebP
-magick "$file" -filter Lanczos -resize "${larghezza_px}x" -quality 90 "$cartella/1x.webp"
-magick "$file" -filter Lanczos -resize "$((larghezza_px * 2))x" -quality 90 "$cartella/2x.webp"
-magick "$file" -filter Lanczos -resize "$((larghezza_px * 3))x" -quality 90 "$cartella/3x.webp"
-magick "$file" -quality 90 "$cartella/full.webp"
+convert "$file" -filter Lanczos -resize "${larghezza_px}x" -quality 90 "$cartella/1x.webp"
+convert "$file" -filter Lanczos -resize "$((larghezza_px * 2))x" -quality 90 "$cartella/2x.webp"
+convert "$file" -filter Lanczos -resize "$((larghezza_px * 3))x" -quality 90 "$cartella/3x.webp"
+convert "$file" -quality 90 "$cartella/full.webp"
 
 # AVIF
-magick "$file" -filter Lanczos -resize "${larghezza_px}x" -quality 90 -define avif:effort=6 "$cartella/1x.avif"
-magick "$file" -filter Lanczos -resize "$((larghezza_px * 2))x" -quality 90 -define avif:effort=6 "$cartella/2x.avif"
-magick "$file" -filter Lanczos -resize "$((larghezza_px * 3))x" -quality 90 -define avif:effort=6 "$cartella/3x.avif"
-magick "$file" -quality 90 -define avif:effort=6 "$cartella/full.avif"
+convert "$file" -filter Lanczos -resize "${larghezza_px}x" -quality 90 -define avif:effort=6 "$cartella/1x.avif"
+convert "$file" -filter Lanczos -resize "$((larghezza_px * 2))x" -quality 90 -define avif:effort=6 "$cartella/2x.avif"
+convert "$file" -filter Lanczos -resize "$((larghezza_px * 3))x" -quality 90 -define avif:effort=6 "$cartella/3x.avif"
+convert "$file" -quality 90 -define avif:effort=6 "$cartella/full.avif"
