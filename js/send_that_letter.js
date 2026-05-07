c59d7aa174f8303e55a0518e810bcfb56097e7dc
Jacopo Donati <33698919+cantaprete@users.noreply.github.com>
2026-05-07 16:52:25 +0200

    Riordino sviluppo

--- js/send_that_letter.js
+++
@@ -1,7 +0,0 @@
-function libriSottoLeGambeDelTavolo() {
-    let tavolo = document.querySelector('#tavolo');
-    tavolo.style.height = window.innerHeight + 'px';
-}
-
-document.addEventListener('DOMContentLoaded', libriSottoLeGambeDelTavolo);
-window.addEventListener('resize', libriSottoLeGambeDelTavolo);
