d56e917a57a1a4014d158fd87f0982a738343292
Jacopo Donati <33698919+cantaprete@users.noreply.github.com>
2026-06-03 10:17:58 +0200

    Liberazione home

--- js/home.js
+++ js/home.js
@@ -1,29 +1,3 @@
-function moveTitle() {
-    let oldHeader = document.querySelector('header')
-    let h1Text = document.querySelector('h1').textContent
-    let oldEmail = document.querySelector('#email')
-    let emailAddress = oldEmail.querySelector('a').getAttribute('href')
-    let emailLabel = oldEmail.textContent
-
-    oldHeader.remove()
-
-    let immissionPoint = document.querySelector('#libri')
-    
-    let newHeader = document.createElement('header')
-    immissionPoint.after(newHeader)
-
-    let newTitle = document.createElement('h1')
-    newTitle.textContent = h1Text
-    newHeader.appendChild(newTitle)
-    oldEmail.remove()
-    let newEmailWrapper = document.createElement('p')
-    newEmailWrapper.id = 'email'
-    let newEmailLink =document.createElement('a')
-    newEmailLink.setAttribute('href', emailAddress)
-    newEmailLink.textContent = emailLabel
-    newEmailWrapper.appendChild(newEmailLink)
-    newTitle.after(newEmailWrapper)
-}
 
 function toggleMenu(e) {
     let opener = document.querySelector('#opener')
@@ -39,8 +13,6 @@ function toggleMenu(e) {
         opener.textContent = '−'
     }
 }
-
-// document.addEventListener('DOMContentLoaded', moveTitle)
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#submenu').addEventListener('click', toggleMenu)
